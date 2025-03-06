9e4a2089ecb9b0aa5f2c57dde9ab4c30ad823773
jicodes <122512755+jicodes@users.noreply.github.com>
2025-03-06 12:50:45 +0800

    add script for creating nft

---
+++ project-06-nfts/create-nft.ts
@@ -0,0 +1,68 @@
+import { Connection, clusterApiUrl, LAMPORTS_PER_SOL } from "@solana/web3.js";
+
+import {
+  airdropIfRequired,
+  getKeypairFromFile,
+  getExplorerLink,
+} from "@solana-developers/helpers";
+
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import {
+  createNft,
+  fetchDigitalAsset,
+  mplTokenMetadata,
+} from "@metaplex-foundation/mpl-token-metadata";
+import { generateSigner, keypairIdentity, percentAmount, publicKey } from "@metaplex-foundation/umi";
+
+
+const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+
+const user = await getKeypairFromFile("user.json");
+
+await airdropIfRequired(
+  connection,
+  user.publicKey,
+  1 * LAMPORTS_PER_SOL,
+  0.5 * LAMPORTS_PER_SOL,
+);
+
+console.log("user public key:", user.publicKey.toBase58());
+
+const umi = await createUmi(connection.rpcEndpoint);
+umi.use(mplTokenMetadata());
+
+const umiUser = umi.eddsa.createKeypairFromSecretKey(user.secretKey);
+umi.use(keypairIdentity(umiUser));
+
+console.log("set up Umi instance for user"); 
+
+const collectionAddress = publicKey(
+  "<YOUR_COLLECTION_ADDRESS_HERE>"
+);
+
+console.log("Creating NFT...");
+
+const mint = generateSigner(umi);
+
+const transaction = createNft(umi, {
+  mint,
+  name: "My NFT",
+  uri: "https://raw.githubusercontent.com/solana-developers/professional-education/main/labs/sample-nft-offchain-data.json",
+  sellerFeeBasisPoints: percentAmount(0),
+  collection: {
+    key: collectionAddress,
+    verified: false,
+  },
+});
+
+await transaction.sendAndConfirm(umi);
+
+const createdNft = await fetchDigitalAsset(umi, mint.publicKey);
+
+console.log(
+  `🖼️ Created NFT! Address is ${getExplorerLink(
+    "address",
+    createdNft.mint.publicKey,
+    "devnet"
+  )}`
+);
