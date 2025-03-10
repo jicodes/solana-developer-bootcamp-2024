ea267b3269525c9bfd3e8232146153fee775f587
jicodes <122512755+jicodes@users.noreply.github.com>
2025-03-10 13:44:37 +0800

    feature: add create employee account instruction and its account validation struct

--- project-08-token-vesting/anchor/programs/vesting/src/lib.rs
+++ project-08-token-vesting/anchor/programs/vesting/src/lib.rs
@@ -10,7 +10,7 @@ pub mod vesting {
     use super::*;
 
     pub fn creating_vesting_account(
-        ctx: Context<CreatingVesting>,
+        ctx: Context<CreatingVestingAccount>,
         company_name: String,
     ) -> Result<()> {
         *ctx.accounts.vesting_account = VestingAccount {
@@ -23,11 +23,32 @@ pub mod vesting {
         };
         Ok(())
     }
+
+    pub fn create_employee_vesting_account(
+        ctx: Context<CreateEmployeeAccount>,
+        cliff_time: i64,
+        start_time: i64,
+        end_time: i64,
+        total_amount: u64,
+    ) -> Result<()> {
+        *ctx.accounts.employee_account = EmployeeAccount {
+            vesting_account: ctx.accounts.vesting_account.key(),
+            beneficiary: ctx.accounts.beneficiary.key(),
+            cliff_time,
+            start_time,
+            end_time,
+            total_amount,
+            total_withdrawn: 0,
+            bump: ctx.bumps.employee_account,
+        };
+
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
 #[instruction(company_name: String)]
-pub struct CreatingVesting<'info> {
+pub struct CreatingVestingAccount<'info> {
     #[account(mut)]
     pub singer: Signer<'info>,
 
@@ -54,6 +75,27 @@ pub struct CreatingVesting<'info> {
     pub token_program: Interface<'info, TokenInterface>,
 }
 
+#[derive(Accounts)]
+pub struct CreateEmployeeAccount<'info> {
+    #[account(mut)]
+    pub owner: Signer<'info>,
+
+    pub beneficiary: SystemAccount<'info>,
+
+    #[account(has_one = owner)]
+    pub vesting_account: Account<'info, VestingAccount>,
+
+    #[account(
+        init,
+        payer = owner,
+        space = 8 + EmployeeAccount::INIT_SPACE,
+        seeds = [b"employee_vesting", beneficiary.key().as_ref(), vesting_account.key().as_ref()],
+        bump
+    )]
+    pub employee_account: Account<'info, EmployeeAccount>,
+    pub system_program: Program<'info, System>,
+}
+
 #[account]
 #[derive(InitSpace)]
 pub struct VestingAccount {
@@ -63,5 +105,18 @@ pub struct VestingAccount {
     #[max_len(50)]
     pub company_name: String,
     pub treasury_bump: u8,
+    pub bump: u8,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct EmployeeAccount {
+    pub vesting_account: Pubkey,
+    pub beneficiary: Pubkey,
+    pub cliff_time: i64,
+    pub start_time: i64,
+    pub end_time: i64,
+    pub total_amount: u64,
+    pub total_withdrawn: u64,
     pub bump: u8,
 }
