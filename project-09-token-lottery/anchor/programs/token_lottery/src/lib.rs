66ca7389fbce2572f17926247218a5ceb4c3b281
jicodes <122512755+jicodes@users.noreply.github.com>
2025-03-16 20:53:06 +0800

    feat: add claim prize instruction and account struct

--- project-09-token-lottery/anchor/programs/token_lottery/src/lib.rs
+++ project-09-token-lottery/anchor/programs/token_lottery/src/lib.rs
@@ -10,6 +10,7 @@ use anchor_spl::{
 
 use anchor_spl::metadata::{
     Metadata,
+    MetadataAccount,
     create_metadata_accounts_v3, 
     CreateMetadataAccountsV3,
     mpl_token_metadata::types::{
@@ -314,6 +315,35 @@ pub mod token_lottery {
 
         Ok(())
     } 
+
+    pub fn claim_prize(ctx: Context<ClaimPrize>) -> Result<()> {
+        // Check if winner has been chosen
+        msg!("Winner chosen: {}", ctx.accounts.token_lottery.winner_chosen);
+        require!(ctx.accounts.token_lottery.winner_chosen, ErrorCode::WinnerNotChosen);
+        
+        // Check if token is a part of the collection
+        require!(ctx.accounts.ticket_metadata.collection.as_ref().unwrap().verified, ErrorCode::NotVerifiedTicket);
+        require!(ctx.accounts.ticket_metadata.collection.as_ref().unwrap().key == ctx.accounts.collection_mint.key(), ErrorCode::IncorrectTicket);
+
+        let ticket_name = NAME.to_owned() + &ctx.accounts.token_lottery.winner.to_string();
+        let metadata_name = ctx.accounts.ticket_metadata.name.replace("\u{0}", "");
+
+        msg!("Ticket name: {}", ticket_name);
+        msg!("Metdata name: {}", metadata_name);
+
+        // Check if the winner has the winning ticket
+        require!(metadata_name == ticket_name, ErrorCode::IncorrectTicket);
+        require!(ctx.accounts.destination.amount > 0, ErrorCode::IncorrectTicket);
+
+        // ** is used to dereference a mutable reference to the lamports of an account.
+        **ctx.accounts.token_lottery.to_account_info().lamports.borrow_mut() -= ctx.accounts.token_lottery.lottery_pot_amount;
+        **ctx.accounts.payer.lamports.borrow_mut() += ctx.accounts.token_lottery.lottery_pot_amount;
+
+        ctx.accounts.token_lottery.lottery_pot_amount = 0;
+
+        Ok(())
+    }
+
 }
 
 #[derive(Accounts)]
@@ -487,7 +517,7 @@ pub struct BuyTicket<'info> {
         associated_token::authority = payer,
         associated_token::token_program = token_program,
     )]
-    pub destination: InterfaceAccount<'info, TokenAccount>,
+    pub destination: InterfaceAccount<'info, TokenAccount>, // ticket_account
 
     pub token_program: Interface<'info, TokenInterface>,
     pub token_metadata_program: Program<'info, Metadata>,
@@ -530,6 +560,57 @@ pub struct RevealWinner<'info> {
     pub randomness_account: UncheckedAccount<'info>,
 }
 
+#[derive(Accounts)]
+pub struct ClaimPrize<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds = [b"token_lottery".as_ref()],
+        bump = token_lottery.bump,
+    )]
+    pub token_lottery: Account<'info, TokenLottery>,
+    
+    #[account(
+        mut,
+        seeds = [b"collection_mint".as_ref()],
+        bump,
+    )]
+    pub collection_mint: InterfaceAccount<'info, Mint>,
+
+    #[account(
+        seeds = [token_lottery.winner.to_le_bytes().as_ref()],
+        bump,
+    )]
+    pub ticket_mint: InterfaceAccount<'info, Mint>,
+
+    #[account(
+        seeds = [b"metadata", token_metadata_program.key().as_ref(), ticket_mint.key().as_ref()],
+        bump,
+        seeds::program = token_metadata_program.key(),
+    )]
+    pub ticket_metadata: Account<'info, MetadataAccount>,
+
+    #[account(
+        associated_token::mint = ticket_mint,
+        associated_token::authority = payer,
+        associated_token::token_program = token_program,
+    )]
+    pub destination: InterfaceAccount<'info, TokenAccount>, //ticket_account
+
+    #[account(
+        mut,
+        seeds = [b"metadata", token_metadata_program.key().as_ref(), collection_mint.key().as_ref()],
+        bump,
+        seeds::program = token_metadata_program.key(),
+    )]
+    pub collection_metadata: Account<'info, MetadataAccount>,
+
+    pub token_program: Interface<'info, TokenInterface>,
+    pub token_metadata_program: Program<'info, Metadata>,
+    pub system_program: Program<'info, System>,
+}
 
 #[account]
 #[derive(InitSpace)]
