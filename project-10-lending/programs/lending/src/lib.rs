1640107b1c37bc81875b7db58b96948fa570a20a
jicodes <122512755+jicodes@users.noreply.github.com>
2025-03-19 16:18:13 +0800

    add instruction for init user account and bank

--- project-10-lending/programs/lending/src/lib.rs
+++ project-10-lending/programs/lending/src/lib.rs
@@ -1,7 +1,9 @@
 use anchor_lang::prelude::*;
 
+mod instructions;
 mod state;
 
+use instructions::*;
 
 declare_id!("7RUayLWiHfhTjsFZndwynddfTaU1r2R8ukn3fVgq238");
 
@@ -9,8 +11,13 @@ declare_id!("7RUayLWiHfhTjsFZndwynddfTaU1r2R8ukn3fVgq238");
 pub mod lending {
     use super::*;
 
+    pub fn init_bank(ctx: Context<InitBank>, liquidation_threshold: u64, max_ltv: u64) -> Result<()> {
+        process_init_bank(ctx, liquidation_threshold, max_ltv)
+    }
 
-    
+    pub fn init_user(ctx: Context<InitUser>, usdc_address: Pubkey) -> Result<()> {
+        process_init_user(ctx, usdc_address)
+    }
 }
 
 #[derive(Accounts)]
