9b4e59b92383fbb6c750654699aa3df22fd38756
jicodes <122512755+jicodes@users.noreply.github.com>
2025-03-19 10:12:09 +0800

    init project 10 lending and add state accounts

---
+++ project-10-lending/programs/lending/src/lib.rs
@@ -0,0 +1,17 @@
+use anchor_lang::prelude::*;
+
+mod state;
+
+
+declare_id!("7RUayLWiHfhTjsFZndwynddfTaU1r2R8ukn3fVgq238");
+
+#[program]
+pub mod lending {
+    use super::*;
+
+
+    
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
