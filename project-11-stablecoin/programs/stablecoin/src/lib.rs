f52603d1262052113f6ec4424e078bb79599c4f0
jicodes <122512755+jicodes@users.noreply.github.com>
2025-03-21 15:27:18 +0800

    feat: add init config instruction

--- project-11-stablecoin/programs/stablecoin/src/lib.rs
+++ project-11-stablecoin/programs/stablecoin/src/lib.rs
@@ -1,16 +1,25 @@
 use anchor_lang::prelude::*;
 
+mod state;
+use state::*;
+
+mod instructions;
+use instructions::*;
+
+mod constants;
+use constants::*;
+
+
+
 declare_id!("9JGTogJJRCqLqKoKP9rSN1yMj1XyQq4VwYVzCvNjWVYQ");
 
 #[program]
 pub mod stablecoin {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
+    pub fn initialize_config(ctx: Context<InitializeConfig>) -> Result<()> {
+        process_initialize_config(ctx)
     }
-}
 
-#[derive(Accounts)]
-pub struct Initialize {}
+    
+}
