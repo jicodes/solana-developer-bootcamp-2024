585fd943e673e2d65e50b175ac459b71856f7c71
jicodes <122512755+jicodes@users.noreply.github.com>
2025-03-22 11:32:18 +0800

    feat: add instruction for redeem collateral and burn tokens

--- project-11-stablecoin/programs/stablecoin/src/instructions/mod.rs
+++ project-11-stablecoin/programs/stablecoin/src/instructions/mod.rs
@@ -4,5 +4,8 @@ pub use admin::*;
 pub mod deposit;
 pub use deposit::*;
 
+pub mod withdraw;
+pub use withdraw::*;
+
 pub mod utils;
 pub use utils::*;
--- project-11-stablecoin/programs/stablecoin/src/instructions/withdraw/utils.rs
+++ project-11-stablecoin/programs/stablecoin/src/instructions/withdraw/utils.rs
@@ -1,48 +1,49 @@
+use crate::SEED_SOL_ACCOUNT;
 use anchor_lang::prelude::*;
 use anchor_lang::system_program::{transfer, Transfer};
 use anchor_spl::{
-    token_2022::{mint_to, MintTo},
+    token_2022::{burn, Burn},
     token_interface::{Mint, Token2022, TokenAccount},
 };
 
-use crate::SEED_MINT_ACCOUNT;
-
-pub fn deposit_sol_internal<'info>(
-    from: &Signer<'info>,
-    to: &SystemAccount<'info>,
+pub fn withdraw_sol_internal<'info>(
+    from: &SystemAccount<'info>,
+    to: &AccountInfo<'info>,
     system_program: &Program<'info, System>,
+    depositor_key: &Pubkey,
+    bump: u8,
     amount: u64,
 ) -> Result<()> {
+    let signer_seeds: &[&[&[u8]]] = &[&[SEED_SOL_ACCOUNT, depositor_key.as_ref(), &[bump]]];
+
     transfer(
-        CpiContext::new(
+        CpiContext::new_with_signer(
             system_program.to_account_info(),
             Transfer {
                 from: from.to_account_info(),
                 to: to.to_account_info(),
             },
+            signer_seeds,
         ),
         amount,
     )
 }
 
-pub fn mint_tokens_internal<'info>(
+pub fn burn_tokens_internal<'info>(
     mint_account: &InterfaceAccount<'info, Mint>,
     token_account: &InterfaceAccount<'info, TokenAccount>,
+    authority: &Signer<'info>,
     token_program: &Program<'info, Token2022>,
-    bump: u8,
     amount: u64,
 ) -> Result<()> {
-    let signer_seeds: &[&[&[u8]]] = &[&[SEED_MINT_ACCOUNT, &[bump]]];
-
-    mint_to(
-        CpiContext::new_with_signer(
+    burn(
+        CpiContext::new(
             token_program.to_account_info(),
-            MintTo {
+            Burn {
                 mint: mint_account.to_account_info(),
-                to: token_account.to_account_info(),
-                authority: mint_account.to_account_info(),
+                from: token_account.to_account_info(),
+                authority: authority.to_account_info(),
             },
-            signer_seeds,
         ),
         amount,
     )
