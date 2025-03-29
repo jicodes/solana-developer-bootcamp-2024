cad317697b63fdf6ee39a884e9a38b26151fe49e
jicodes <122512755+jicodes@users.noreply.github.com>
2025-03-29 17:12:04 +0800

    fix: optimize priority fee limit and price for sending vote transactions

--- project-13-getting-to-production/web/components/voting/voting-data-access.tsx
+++ project-13-getting-to-production/web/components/voting/voting-data-access.tsx
@@ -1,46 +1,86 @@
-'use client';
+"use client";
 
-import { getVotingProgram, getVotingProgramId } from '@voting-dapp/anchor';
-import { Program, BN } from '@coral-xyz/anchor';
-import { useConnection } from '@solana/wallet-adapter-react';
-import { Cluster, PublicKey } from '@solana/web3.js';
-import { useMutation, useQuery } from '@tanstack/react-query';
-import { useMemo } from 'react';
-import toast from 'react-hot-toast';
-import { useCluster } from '../cluster/cluster-data-access';
-import { useAnchorProvider } from '../solana/solana-provider';
-import { useTransactionToast } from '../ui/ui-layout';
+import { getVotingProgram, getVotingProgramId } from "@voting-dapp/anchor";
+import { Program, BN } from "@coral-xyz/anchor";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import {
+  Cluster,
+  PublicKey,
+  ComputeBudgetProgram,
+  Transaction,
+} from "@solana/web3.js";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMemo } from "react";
+import toast from "react-hot-toast";
+import { useCluster } from "../cluster/cluster-data-access";
+import { useAnchorProvider } from "../solana/solana-provider";
+import { useTransactionToast } from "../ui/ui-layout";
 
-
-export function useVotingProgramCandidateAccount({ account }: { account: PublicKey }) {
+export function useVotingProgramCandidateAccount({
+  account,
+}: {
+  account: PublicKey;
+}) {
   const { cluster } = useCluster();
   const provider = useAnchorProvider();
   const program = getVotingProgram(provider);
+  const { connection } = useConnection();
+  const { publicKey, sendTransaction } = useWallet();
   const transactionToast = useTransactionToast();
 
   const candidateQuery = useQuery({
-    queryKey: ['candidate', { cluster, account }],
+    queryKey: ["candidate", { cluster, account }],
     queryFn: () => program.account.candidate.fetch(account),
   });
 
   const vote = useMutation({
-    mutationKey: ['voting', 'vote', { cluster }],
-    mutationFn: (candidate: string) =>
-      program.methods.vote(
-        candidate,
-        new BN(1),
-      ).rpc(),
-      onSuccess: (signature) => {
-        transactionToast(signature);
-        return candidateQuery.refetch();
-      },
-      onError: () => toast.error('Failed to vote for candidate'),
+    mutationKey: ["voting", "vote", { cluster }],
+    mutationFn: async (candidate: string) => {
+      const modifyComputeUnits = ComputeBudgetProgram.setComputeUnitLimit({
+        units: 8000,
+      });
+
+      const recentPriorityFees = await connection.getRecentPrioritizationFees({
+        lockedWritableAccounts: [
+          new PublicKey("CGginWZDcCcApmApSmjABgXWKUg6rKL4JrCDXkj7zvTi"),
+        ],
+      });
+      const minFee = Math.min(
+        ...recentPriorityFees.map((fee) => fee.prioritizationFee),
+      );
+
+      const addPriorityFee = ComputeBudgetProgram.setComputeUnitPrice({
+        microLamports: minFee + 1,
+      });
+
+      const voteIx = await program.methods
+        .vote(candidate, new BN(1))
+        .instruction();
+
+      const blockhashContext = await connection.getLatestBlockhashAndContext();
+
+      const transaction = new Transaction({
+        feePayer: publicKey,
+        blockhash: blockhashContext.value.blockhash,
+        lastValidBlockHeight: blockhashContext.value.lastValidBlockHeight,
+      })
+        .add(modifyComputeUnits)
+        .add(addPriorityFee)
+        .add(voteIx);
+
+      return await sendTransaction(transaction, connection);
+    },
+    onSuccess: (signature) => {
+      transactionToast(signature);
+      return candidateQuery.refetch();
+    },
+    onError: () => toast.error("Failed to vote for candidate"),
   });
 
   return {
     candidateQuery,
-    vote
-  }
+    vote,
+  };
 }
 
 export function useVotingProgram() {
@@ -50,42 +90,39 @@ export function useVotingProgram() {
   const provider = useAnchorProvider();
   const programId = useMemo(
     () => getVotingProgramId(cluster.network as Cluster),
-    [cluster]
+    [cluster],
   );
   const program = getVotingProgram(provider);
 
   const accounts = useQuery({
-    queryKey: ['voting', 'all', { cluster }],
+    queryKey: ["voting", "all", { cluster }],
     queryFn: () => program.account.candidate.all(),
   });
 
   const polls = useQuery({
-    queryKey: ['polls', 'all', { cluster }],
+    queryKey: ["polls", "all", { cluster }],
     queryFn: () => program.account.poll.all(),
   });
 
   const candidates = useQuery({
-    queryKey: ['candidates', 'all', { cluster }],
+    queryKey: ["candidates", "all", { cluster }],
     queryFn: () => program.account.candidate.all(),
   });
 
   const getProgramAccount = useQuery({
-    queryKey: ['get-program-account', { cluster }],
+    queryKey: ["get-program-account", { cluster }],
     queryFn: () => connection.getParsedAccountInfo(programId),
   });
 
   const vote = useMutation({
-    mutationKey: ['voting', 'vote', { cluster }],
+    mutationKey: ["voting", "vote", { cluster }],
     mutationFn: (candidate: string) =>
-      program.methods.vote(
-        candidate,
-        new BN(1),
-      ).rpc(),
-      onSuccess: (signature) => {
-        transactionToast(signature);
-        return accounts.refetch();
-      },
-      onError: () => toast.error('Failed to vote for candidate'),
+      program.methods.vote(candidate, new BN(1)).rpc(),
+    onSuccess: (signature) => {
+      transactionToast(signature);
+      return accounts.refetch();
+    },
+    onError: () => toast.error("Failed to vote for candidate"),
   });
 
   return {
@@ -95,6 +132,6 @@ export function useVotingProgram() {
     polls,
     getProgramAccount,
     vote,
-    candidates
+    candidates,
   };
 }
